7f60eea67038a333599f6cec1b4f1d9d3b9f72bc
Konstantinos Theodoropoulos <konstantinostheodoropoulosphoto@hotmail.com>
2023-02-25 16:19:45 +0100

    Complited the toggling of the contact form and the todolist😀

--- backend/script.js
+++ backend/script.js
@@ -1,12 +1,15 @@
 // create a function, that pull the input value from the input field, and then package the text in a textNode, to send the text that is wrapped with TextNode to an empty JS created ListItem, so then the ListItem with its TextNode child, can be sent as a whole package to the UL.
 let todoListUl = document.querySelector(".myUl");
 let todoListWrapper = document.getElementById("TodoListWrapper");
+let ContactFormWraper = document.getElementById("contact_form");
 
 let ServiceId = "service_bhytemr";
 let TemplateId = "template_ii54qll";
 let publicApiId = "yZ9gB5CTCdFCk586e";
 let formSubmitButton = document.querySelector(".sbmtButton");
 let theForm = document.getElementById("contact_form");
+
+let currentComponent ="TodoList";
 
 
 //This algorithm is used in the TodoList
@@ -39,10 +42,35 @@ theForm.addEventListener("submit", (e) => {
 });
 
 //Show and Hide TodoList
-function ShowTodos() {
-    if (todoListWrapper.style.display === "none") {
-        todoListWrapper.style.display = "block"
-    } else {
-        todoListWrapper.style.display = "none";
+// function ShowTodos() {
+//     if (todoListWrapper.style.display === "none") {
+//         todoListWrapper.style.display = "block"
+//     } else {
+//         todoListWrapper.style.display = "none";
+//     }
+// }
+//
+// function ShowForm() {
+//     if (ContactFormWraper.style.display === "none") {
+//         ContactFormWraper.style.display = "block"
+//     } else {
+//         ContactFormWraper.style.display = "none";
+//     }
+//
+//
+// }
+function ShowTodos(){
+    if(currentComponent!=="TodoList"){
+        todoListWrapper.style.display="block";
+        ContactFormWraper.style.display ="none";
+        currentComponent ="TodoList";
+    }
+}
+
+function ShowForm(){
+    if(currentComponent!=="contactForm"){
+        ContactFormWraper.style.display="block";
+        todoListWrapper.style.display ="none";
+        currentComponent ="contactForm";
     }
 }
